05e8e0ab1e87f9c84ef9ea614472a90500e3d1a0
circlefire <jwy0148@naver.com>
2023-03-06 17:54:11 +0900

    study: 데이터베이스 첫 수업 코드

---
+++ index_01.sql
@@ -0,0 +1,11 @@
+-- 현재 존재하는 데이터베이스를 확인하는 명령어. 사실 안해도 되는 명령어
+show databases;
+
+-- smartfactory 데이터베이스를 사용하는 명령어. 얘는 무조건 써야하는 명령어
+use smartfactory;
+
+-- customer 테이블의 모든 내용 검색
+SELECT * FROM customer;
+
+-- orders 테이블의 모든 내용 검색
+SELECT * FROM orders;
