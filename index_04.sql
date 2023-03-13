d9fb3677ae9c3832c15d5879d65ee77f1dea3c2d
circlefire <jwy0148@naver.com>
2023-03-13 17:33:55 +0900

    study: 11주차실습

--- index_04.sql
+++ index_04.sql
@@ -1,5 +1,5 @@
 -- DML: SELECT, INSERT, UPDATE, DELETE
-use new_smartfactory;
+use smartfactory;
 CREATE TABLE new_customer (
 	custid CHAR(10) PRIMARY KEY,
     custname VARCHAR(10) NOT NULL, 
@@ -8,6 +8,96 @@ CREATE TABLE new_customer (
     birth DATE
 );
 DESC new_customer;
+use new_prac;
+create table authors(
+	author_id int not null primary key,
+	first_name varchar(50),
+    last_name varchar(50),
+    email varchar(50)
+);
+create table books(
+	book_id int not null primary key,
+    author_id int,
+	title varchar(100),
+    publication_date date,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+create table orders(
+	order_id int not null primary key,
+	customer_name varchar(50),
+    book_id int,
+    FOREIGN KEY(book_id) REFERENCES books(book_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    order_date date
+);
+INSERT INTO authors VALUES 
+(1,'J.K','Rowling','jkrowling@gmail.com'),
+(2,'George R.R','Martin','grmartin@yahoo.com'),
+(3,'Stephen','King','sking@hotmail.com');
+
+DESC authors;
+SELECT * FROM authors;
+
+INSERT INTO books VALUES 
+(1,1,'Harry Potter and the Philospher''s Stone','1997-06-26'),
+(2,2,'A Game of Thrones','1996-08-06'),
+(3,3,'The Shining','1977-01-28');
+
+DESC books;
+SELECT * FROM books;
+
+INSERT INTO orders VALUES 
+(1,'John Smith',1,'2022-02-15'),
+(2,'Jane Doe',2,'2022-02-16'),
+(3,'Bob Johnson',3,'2022-02-17');
+DESC orders;
+SELECT * FROM orders;
+-- 1. author_id가 1인 작성자의 이메일을 jkrowling@yahoo.com으로 업데이트하는 SQL 문을 작성합니다.
+update authors set email = 'jkrowling@yahoo.com' where author_id=1;
+
+-- 2. books 테이블에서 book_id 2인 책을 삭제하는 SQL 문을 작성합니다.
+delete from books where book_id=2;
+
+-- 3. 다음 세부 정보가 포함된 새 책을 삽입하는 SQL 문을 작성합니다.
+-- 책 ID: 4
+-- 제목: 스탠드
+-- 저자 ID: 3
+-- 발행일자 : 1978-01-01
+insert into books values (4,3,'스탠드','1978-01-01');
+
+-- 4. book_id 1인 책의 출판 날짜를 1997-06-30으로 업데이트하는 SQL 문을 작성하십시오.
+update books set publication_date = '1997-06-30' where book_id=1;
+
+-- 5. 022-02-17 이전에 접수된 모든 주문을 삭제하는 SQL 문을 작성합니다.
+SET sql_safe_updates=0;
+delete from orders where order_date<'2022-02-17';
+
+-- 6. 다음 세부 정보와 함께 새 주문을 삽입하는 SQL 문을 작성합니다.
+-- 주문 ID: 4
+-- 책 ID: 1
+-- 고객 이름: 사라 존슨
+-- 주문일자 : 2022-02-18
+insert into orders values (4,'사라 존슨',1,'2022-02-18');
+
+
+-- 7. order_id가 1인 주문의 고객 이름을 Jack Smith로 업데이트하는 SQL 문을 작성합니다.
+update orders set customer_name = 'Jack Smith' where order_id=1;
+
+
+-- 8. 다음 세부 정보와 함께 새 작성자를 삽입하는 SQL 문을 작성합니다.
+-- 저자 ID: 4
+-- 이름: 아가사
+-- 성: 크리스티
+-- 이메일: agatha.christie@example.com
+insert into authors values (4,'아가사','크리스티','agatha.christie@example.com');
+
+
+-- 9. author_id 2인 작성자의 성을 Martinez로 업데이트하는 SQL 문을 작성합니다.
+update authors set  last_name = 'Martinez' where author_id=2;
+
+-- 10. author_id 3인 저자가 쓴 모든 책을 삭제하는 SQL 문을 작성합니다.
+delete from authors where author_id=3;
+
+
 
 CREATE TABLE new_orders (
 	orderid INT PRIMARY KEY AUTO_INCREMENT,
